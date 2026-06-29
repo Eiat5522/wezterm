60a25d3bb2082ff996a55405961e02039c17fdd7
Eiat5522 <eiat52@gmail.com>
2026-06-30 00:11:20 +0700

    feat(fonts): change primary font.

--- config/fonts.lua
+++ config/fonts.lua
@@ -2,15 +2,20 @@ local wezterm = require("wezterm")
 local M = {}
 
 function M.apply_to_config(config)
-	config.font_size = 16
+	config.font_size = 15
 	config.font = wezterm.font_with_fallback({
 		{
-			family = "JetBrains Mono",
+			family = "CaskaydiaCove NF",
 			weight = "Medium",
 		},
 		{
-			family = "Terminus",
+			family = "CaskaydiaCove NF",
 			weight = "Bold",
+		},
+		{
+			family = "CaskaydiaCove NF",
+			weight = "Light",
+			style = "Italic",
 		},
 		family = "Noto Color Emoji",
 	})
